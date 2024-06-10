4503c1689b58ebb52bdc2eca5ab299bcc471f91a
Mohammed Saqlain <md.saqlain3100@gmail.com>
2024-06-10 21:36:21 +0530

    Build the new Issue Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import NavBar from './NavBar'
+import{ Button } from '@radix-ui/themes'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main>
-      <p>Home</p>
+      <p><Button><Link href='/issues/new'>New Issues</Link></Button></p>
     </main>
   )
 }
